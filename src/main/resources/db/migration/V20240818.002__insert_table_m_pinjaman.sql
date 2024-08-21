a85d64f93a4915cee6b4ad99bda75504e2efea00
devam <devamahesa99@gmail.com>
2024-08-21 08:19:18 +0700

    push unrestricted file

---
+++ src/main/resources/db/migration/V20240818.002__insert_table_m_pinjaman.sql
@@ -0,0 +1,99 @@
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(1, 'BCAFINANCE', 87406000, 12, 21435000, 200, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(2, 'BCAFINANCE', 89233000, 24, 11511000, 200, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(3, 'BCAFINANCE', 90947000, 36, 8213000.0, 200, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(4, 'BCAFINANCE', 92603000, 48, 6659000.0, 200, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(5, 'BCAFINANCE', 94320000, 60, 5861000.0, 200, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(6, 'BCAFINANCE', 81407000, 12, 19960000, 105, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(7, 'BCAFINANCE', 83117000, 24, 10720000, 105, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(8, 'BCAFINANCE', 84721000, 36, 7649000.0, 105, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(9, 'BCAFINANCE', 86389000, 48, 6201000.0, 105, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(10, 'BCAFINANCE', 87996000, 60, 5458000.0, 105, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(11, 'BCAFINANCE', 72664000, 12, 17813000, 92, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(12, 'BCAFINANCE', 74197000, 24, 9567000.0, 92, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(13, 'BCAFINANCE', 75749000, 36, 6826000.0, 92, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(14, 'BCAFINANCE', 77245000, 48, 5534000.0, 92, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(15, 'BCAFINANCE', 78687000, 60, 4871000.0, 92, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(16, 'BCAFINANCE', 77360000, 12, 18966000, 122, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(17, 'BCAFINANCE', 78993000, 24, 10185000, 122, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(18, 'BCAFINANCE', 80645000, 36, 7267000.0, 122, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(19, 'BCAFINANCE', 82238000, 48, 5892000.0, 122, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(20, 'BCAFINANCE', 83773000, 60, 5186000.0, 122, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(21, 'BCAFINANCE', 45795204, 11, 8297800.0, 8, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(22, 'BCAFINANCE', 45220112, 23, 4317100.0, 8, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(23, 'BCAFINANCE', 47539116, 35, 2990200.0, 8, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(24, 'BCAFINANCE', 50341220, 47, 2386800.0, 8, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(25, 'BCAFINANCE', 49324196, 11, 8925800.0, 14, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(26, 'BCAFINANCE', 48705688, 23, 4643900.0, 14, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(27, 'BCAFINANCE', 51200084, 35, 3216500.0, 14, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(28, 'BCAFINANCE', 54214480, 47, 2567500.0, 14, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(29, 'BCAFINANCE', 22350000, 12, 10209000, 176, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(30, 'BCAFINANCE', 22350000, 24, 5593000.0, 176, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(31, 'BCAFINANCE', 22600000, 36, 4093000.0, 176, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(32, 'BCAFINANCE', 22600000, 48, 3518000.0, 176, '2024-08-18 21:12:00.000', NULL, NULL);
+INSERT INTO pengajuan.m_pinjaman
+(id_pinjaman, asuransi, down_payment, lama_kredit, angsuran, id_kendaraan, created_at, updated_at, deleted_at)
+VALUES(33, 'BCAFINANCE', 22600000, 60, 3202000.0, 176, '2024-08-18 21:12:00.000', NULL, NULL);
